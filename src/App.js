3f2af0064d94c55a88046bdde25af442e4f2d939
cypressg <krasauskas.kipras@gmail.com>
2021-12-09 12:39:03 +0200

    Changed order of component Navigation and SearchBar

--- src/App.js
+++ src/App.js
@@ -6,11 +6,12 @@ import logo from "./images/logo.svg";
 const App = () => {
   return (
     <div>
+      <Navigation img={logo} />
+
       <SearchBar
         title="Testų kūrimo ir spendimo sistema"
         description="Kurkite, spręskite ir dalinkitės įvairiausiais testais, kontroliniais darbais ir klausimynais vienoje vietoje"
       ></SearchBar>
-      <Navigation img={logo} />
     </div>
   );
 };
