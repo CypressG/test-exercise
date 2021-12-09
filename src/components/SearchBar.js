617acccff1d9e3e509172627ebbc8ac5b4465478
JokubasM <jokubas45@gmail.com>
2021-12-09 12:52:39 +0200

    input added in SearchBar.js

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,6 +3,7 @@ const searchBar = (props) => {
     <div className="searchbar-container">
       <h1>{props.title}</h1>
       <p>{props.description}</p>
+      <input>{props.bar}</input>
     </div>
   );
 };
